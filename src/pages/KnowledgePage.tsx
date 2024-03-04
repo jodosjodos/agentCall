87fa5844e7dda9170011831955bb94c3561465e6
jodosjodos <jeandedieu2030@gmail.com>
2024-03-04 18:31:18 +0200

    one model

--- src/pages/KnowledgePage.tsx
+++ src/pages/KnowledgePage.tsx
@@ -1,6 +1,7 @@
-import { Col,  Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import styled from "styled-components";
 import "./int.css";
+import { SalesPitchers } from "../component/SalesPitchers";
 const H1Styled = styled.h4`
   color: #96adb3;
   font-weight: bold;
@@ -58,23 +59,47 @@ const ComponentDiv = styled.div`
   color: #96adb3;
 `;
 const ParentProgressBar = styled.div`
-  border:2px solid #09BED7;
-  border-radius:20px;
-  padding:5px;
+  border: 2px solid #09bed7;
+  border-radius: 20px;
+  padding: 5px;
 `;
 const ChildProgressBar = styled.div`
-  background-color: #09BED7;
-  border-radius:20px;
- color:black;
- width:90%;
- padding-inline:5px;
-
+  background-color: #09bed7;
+  border-radius: 20px;
+  color: black;
+  width: 90%;
+  padding-inline: 5px;
 `;
 
 // sales
 const SalesCol = styled(Col)`
-background-color:#0F2E35;
-`
+  background-color: #0b2227;
+  border-radius: 10px;
+`;
+
+const LinkP = styled.p`
+  color: #c9d5d8;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+const ComponentRender = styled.button`
+  border: 2px solid #96adb3;
+  color: #96adb3;
+  background-color: transparent;
+  border-radius: 25px;
+  &:hover {
+    background-color: #4a666c;
+    border-color: #96adb3;
+  }
+  &:active {
+    background-color: #4a666c;
+    border-color: #96adb3;
+  }
+`;
+const UnderLineSpan = styled.span`
+  text-decoration: underline;
+`;
 export function KnowledgePage() {
   return (
     <Col>
@@ -82,7 +107,7 @@ export function KnowledgePage() {
         <H1Styled>Welcome Raam , Adi</H1Styled>
         <PModified>September 12, 2024</PModified>
       </DivStyled>
-      <Row sm={3} className="pt-3 px-5  ">
+      <Row sm={3} className="pt-3 px-5 ">
         <FileCol lg={3} className="p-2 d-flex flex-column gap-3">
           <StyledH4>All files</StyledH4>
           {/* parental one */}
@@ -141,20 +166,31 @@ export function KnowledgePage() {
           </div>
           <div>
             <p>90/100files</p>
-            {/* not done yet */}
-            {/* <ProgressBarChild
-              min={0}
-              max={100}
-              now={90}
-              label={`${90}%`}
-              variant="awesome"
-            ></ProgressBarChild> */}
+
             <ParentProgressBar>
               <ChildProgressBar className="text-end">90%</ChildProgressBar>
             </ParentProgressBar>
           </div>
         </FileCol>
-        <SalesCol lg={9}>sales pitches</SalesCol>
+        <SalesCol lg={9} className="d-flex flex-column gap-5 pt-3">
+          <LinkP className="text-end">
+            + <UnderLineSpan> upload Document</UnderLineSpan>
+          </LinkP>
+          <div className="d-flex flex-column gap-3">
+            <div className="d-flex flex-row gap-3">
+              <ComponentRender className="py-2 px-3">
+                Sales Pitches
+              </ComponentRender>
+              <ComponentRender className="py-2 px-3">
+                Product Knowledge
+              </ComponentRender>
+              <ComponentRender className="py-2 px-3">
+                Service guidelines
+              </ComponentRender>
+            </div>
+            <SalesPitchers />
+          </div>
+        </SalesCol>
       </Row>
     </Col>
   );
