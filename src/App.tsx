5c5e1bb4d30dbf0b1ad131b25568c07b6f2d357d
jodosjodos <jeandedieu2030@gmail.com>
2024-03-04 07:39:42 +0200

    add knowledge router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/Home";
+import { KnowledgePage } from "./pages/Knowledge";
 function App() {
   return (
     <div>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/Knowledge" element={<KnowledgePage />} />
         </Routes>
       </Router>
     </div>
