542f1450a3d0ebf311262be41146e8d38e51bde4
ISAMAZA Sylvain <isamazasylvin@gmail.com>
2024-03-13 21:27:35 +0200

    working on modal

--- src/component/agents/OnGoingCall.tsx
+++ src/component/agents/OnGoingCall.tsx
@@ -6,6 +6,8 @@ const GreenContainer = styled.div`
   border-radius: 16px;
   background-color: rgba(38, 246, 96, 0.15);
   padding: 4px 8px;
+  display: flex;
+
   color: rgba(16, 122, 71, 1);
 `;
 
@@ -21,7 +23,12 @@ function OngoingCallModal(props: any) {
     >
       <div className="">
         <div className="d-flex p-2  justify-content-between align-items-center">
-          <GreenContainer>Ongoing call with +84965482487</GreenContainer>
+          <div className="d-flex gap-1">
+            <GreenContainer className="gap-1">
+              <img src="/green-wave.svg" alt="" />
+              Ongoing call with +84965482487
+            </GreenContainer>
+          </div>
           <img
             src="/close.svg"
             alt=""
